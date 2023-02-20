5310c1592a3f60adedd2aa602d5c4e6c364615e7
git账号 <3185746516@qq.com>
2023-02-20 17:31:03 +0800

    feat: 通过Controller模拟消息的生产者

---
+++ src/main/java/org/example/controller/DirectController.java
@@ -0,0 +1,32 @@
+package org.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.config.DirectExchangeConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/direct")
+public class DirectController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public DirectController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    /**
+     * direct交换机为直连模式交换机
+     * 根据消息携带的路由键将消息投递给对应队列
+     *
+     * @return
+     */
+    @GetMapping("send")
+    public Object sendMsg() {
+        rabbitTemplate.convertAndSend(DirectExchangeConfig.DIRECT_EXCHANGE, DirectExchangeConfig.DIRECT_ROUTING_KEY, "发送一条测试消息：direct");
+        return "direct消息发送成功！！";
+    }
+}
