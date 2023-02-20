141a6483ee6695b71d19565f6a2140fb952816d1
git账号 <3185746516@qq.com>
2023-02-20 17:30:00 +0800

    feat: 配置声明交换机，队列和绑定关系

--- src/main/java/org/example/config/WebMvcConfig.java
+++ src/main/java/org/example/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ package org.example.config;
 import org.example.Interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -24,6 +25,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 // 排除 login请求
                 .excludePathPatterns("/login");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
     }
 }
 
